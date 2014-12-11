3353e6e5750939e53c218a636449b4bb26486c87
Kazutaka Sankai <Sankai_Kazutaka@ogis-ri.co.jp>
2014-12-11 05:38:29 +0000

    befor commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  
+  #before_actionを設定
   before_action :authenticate_user!
 
   #before_filterを設定
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-protected
+  protected
 
-def configure_permitted_parameters
-  #strong parametersを設定し、useridを許可
-  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:userid, :password, :password_confirmation, :email) }
+  def configure_permitted_parameters
+    #strong parametersを設定し、useridを許可
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:userid, :email, :password, :password_confirmation, :email) }
+  end
 
-end
+  end
